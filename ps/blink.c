283ec18b879f8a985727f3d4470795ca9e3c7906
Maximilian Kirschner <maxi.kirschner98@gmail.com>
2024-09-25 14:34:16 +0000

    Comments and simplyfied blink program in PS example

--- ps/blink.c
+++ ps/blink.c
@@ -9,21 +9,23 @@
 
 #define GPIO0_PIN7           0x80
 
+// Simple delay function with busy loop
 void delay(uint32_t delay) {
   for(volatile uint32_t i = 0; i < delay; i++);
 }
 
 int main(void)
 {
+  // Initialize GPIO0_PIN7 with some default values
   MIO_PIN07_CTRL = MIO_PIN07_CTRL_GPIO0;
 
+  // Set GPIO0_PIN7 as output
   GPIO_DIRM0 |= GPIO0_PIN7;
+  // Enable output on GPIO0_PIN7
   GPIO_OEN0  |= GPIO0_PIN7;
 
   while(1) {
-    GPIO_DATA0 &= ~GPIO0_PIN7;
-    delay(1000000);
-    GPIO_DATA0 |= GPIO0_PIN7;
+    GPIO_DATA0 ^= GPIO0_PIN7;
     delay(1000000);
   }
 }
